0918e03de26d806ca64e34e16098d6a0e3b7acf5
javelamanuel@gmail.com <javelamanuel@gmail.com>
2026-08-21 21:07:54 -0300

    fix: corrige caminho dos icones e regex do cache PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-
 export default defineConfig({
   plugins: [react(), VitePWA({
       registerType: 'autoUpdate',
@@ -13,37 +12,30 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-
         icons: [
           {
-            src: '/public/favicon-192x192.png',
+            src: '/favicon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/public/favicon-512x512.png',
+            src: '/favicon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ]
       },
-
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: '/^https:\/\/semdesperdicio\.smartru\.com\.br\/api\/.*/i',
-
+            urlPattern: /^https:\/\/semdesperdicio\.smartru\.com\.br\/api\/.*/i,
             handler: 'NetworkFirst',
-
             options: {
               cacheName: 'smartru-api-cache',
-
               networkTimeoutSeconds: 5,
-
               cacheableResponse: {
                 statuses: [0, 200]
               },
-
               expiration: {
                 maxEntries: 100,
                 maxAgeSeconds: 60 * 60 * 24
